a55c084cd8be6f0fcce59646e95db6844c00a02b
Arian Nurrifqhi <Arian Nurrifqhi>
2018-03-18 15:10:54 +0700

    v.1.1.1

--- application/views/dekan/datalkd_v.php
+++ application/views/dekan/datalkd_v.php
@@ -80,6 +80,12 @@
                     </div>
                 </div><br>
             <!-- </div> -->
+            <style media="screen">
+            .modal-body {
+              max-height: calc(100vh - 260px);
+              overflow-y: auto;
+            }
+            </style>
             <div class="modal inmodal" id="myModalEdit" tabindex="-1" role="dialog"  aria-hidden="true">
                             <div class="modal-dialog modal-lg">
                                 <div class="modal-content animated fadeInDown">
