a7d4a751cc6f7306edc4d775f1983f69f60abe24
Arian Nurrifqhi <Arian Nurrifqhi>
2018-03-21 23:08:12 +0700

    lkd v.1.2.1

--- application/views/dosen/laporanlkd_v.php
+++ application/views/dosen/laporanlkd_v.php
@@ -19,9 +19,6 @@
             <div class="row">
               <div class="ibox-content col-lg-12">
                 <div class="row">
-                        <div class="col-md-2">
-                          <h2>Form</h2>
-                        </div>
                         <div class=" col-md-3" style="margin-top:5px;margin-bottom:-10px;">
                           <label for="semester">Filter Mingguan</label>
                           <div class="form-group">
